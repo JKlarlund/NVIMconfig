51325c38830a060d76dd740f9ca05e8b9fab9ec9
Jonathan Rønnow Klarlund <jonathanklarlund@outlook.com>
2025-02-13 19:18:15 +0300

    Changes theme (still named gruvbox), added line numbers, setup language server for F#

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,8 +1,1 @@
-return{
-  "ellisonleao/gruvbox.nvim", 
-  name = "gruvbox", 
-  priority = 1000, 
-  config = function()
-    vim.cmd.colorscheme "gruvbox"
-  end
-}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
